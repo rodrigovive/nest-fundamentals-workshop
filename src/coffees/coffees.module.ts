82d1eb848988e6697a3b9aff42e247bddb7cf892
rodrigo_viveros <cherry199662@gmail.com>
2020-12-27 18:03:58 -0500

    feat: flavor many to many

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,10 +3,11 @@ import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 // decorater that provides nest operation metadata
 // organize operation structure
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
